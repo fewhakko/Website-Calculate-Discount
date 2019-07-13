3038ef9af979ea6999529929b64fc7b971839d63
FewHakko <41436874+FewHakko@users.noreply.github.com>
2019-07-13 21:52:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,117 +1,99 @@
 <html lang="en">
-    <head>
-        <title>Few Dev</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="stylesheet" href="asset/css/bootstrap.min.css">
-
-        <style>
-            @import url('https://fonts.googleapis.com/css?family=Kanit');
-
-            body {
-                font-family: 'Kanit', sans-serif;
-            }
-        </style>
-
-    </head>
-  <body>
-            <div class="navbar navbar-expand-lg fixed-top navbar-light bg-light">
-                <div class="container">
-                    <a href="../" class="navbar-brand">คำนวนเปอร์เซ็น</a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                        <a class="nav-link" href="https://www.facebook.com/Few.Hakkoz">facebook</a>
-                        </li>
-                    </ul>
-
-                    <ul class="nav navbar-nav ml-auto">
-                        <li class="nav-item">
-                        <a class="nav-link" href="https://wrapbootstrap.com/?ref=bsw" target="_blank">WrapBootstrap</a>
-                        </li>
-                    </ul>
-
-                    </div>
-                </div>
+   <head>
+      <title>Few Dev</title>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <link rel="stylesheet" href="asset/css/bootstrap.min.css">
+      <style>
+         @import url('https://fonts.googleapis.com/css?family=Kanit');
+         body {
+         font-family: 'Kanit', sans-serif;
+         }
+      </style>
+   </head>
+   <body>
+      <div class="navbar navbar-expand-lg fixed-top navbar-light bg-light">
+         <div class="container">
+            <a href="../" class="navbar-brand">คำนวนเปอร์เซ็น</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+               <ul class="navbar-nav">
+                  <li class="nav-item">
+                     <a class="nav-link" href="https://www.facebook.com/Few.Hakkoz">facebook</a>
+                  </li>
+               </ul>
+               <ul class="nav navbar-nav ml-auto">
+                  <li class="nav-item">
+                     <a class="nav-link" href="https://wrapbootstrap.com/?ref=bsw" target="_blank">WrapBootstrap</a>
+                  </li>
+               </ul>
             </div>
-            
-            <div class="container">
-
-            <br>
-
-                <div class="card text-dark bg-light mb-3" style="margin-top : 100px;">
-                <div class="card-header">เครื่องมือคำนวณการค้นหา%</div>
-                <div class="card-body">
-
-    
-
-
-                  <div class="panel panel-info">
-                    <div class="panel-body">
-                    <form class="form-horizontal">
-
-                    <div class="alert alert-dismissible alert-warning">
-                        <button type="button" class="close" data-dismiss="alert">&times;</button>
-                        <h4 class="alert-heading">คำอธิบาย</h4>
-                        <p class="mb-0"> </b>ตัวช่วยคำนวณค้นหาคำตอบของ%ของตัวเลขที่เราอยากรู้ เช่น 3% ของ 1000 กดคำนวณออกมา คำตอบจะได้ 970 
-                    <br>
-                    ตัวอย่างการใช้เครื่องช่วยคำนวณ :ค่าธรรมเนียม 14.40% ของเงินต้น 1,000 บาท จะต้องเสียค่าธรรมเนียมกี่บาท กดคำนวณออกมา คำตอบจะได้ 856 </p>
-                    </div>
-
-                    <br>
-
-                    <div class="row">
-                        <div class="col-lg-4 offset-lg-1">
-                            <div class="form-group">
-                                <label class="control-label">จำนวนเงิน</label>
-                                <div class="form-group">
-                                <div class="input-group mb-3">
-                                    <div class="input-group-prepend">
-                                    <span class="input-group-text">฿</span>
-                                    </div>
-                                    <input type="text" id="money" class="form-control" placeholder="10000">
-                                    <div class="input-group-append">
-                                    <span class="input-group-text">.00</span>
-                                    </div>
-                                </div>
-                                </div>
-                            </div>
+         </div>
+      </div>
+      <div class="container">
+         <br>
+         <div class="card text-dark bg-light mb-3" style="margin-top : 100px;">
+            <div class="card-header">เครื่องมือคำนวณการค้นหา%</div>
+            <div class="card-body">
+               <div class="panel panel-info">
+                  <div class="panel-body">
+                     <form class="form-horizontal">
+                        <div class="alert alert-dismissible alert-warning">
+                           <button type="button" class="close" data-dismiss="alert">&times;</button>
+                           <h4 class="alert-heading">คำอธิบาย</h4>
+                           <p class="mb-0"> </b>ตัวช่วยคำนวณค้นหาคำตอบของ%ของตัวเลขที่เราอยากรู้ เช่น 3% ของ 1000 กดคำนวณออกมา คำตอบจะได้ 970 
+                              <br>
+                              ตัวอย่างการใช้เครื่องช่วยคำนวณ :ค่าธรรมเนียม 14.40% ของเงินต้น 1,000 บาท จะต้องเสียค่าธรรมเนียมกี่บาท กดคำนวณออกมา คำตอบจะได้ 856 
+                           </p>
                         </div>
-
-                        <div class="col-lg-4 offset-lg-2">
-                            <div class="form-group">
-                                <label class="control-label">ส่วนลดเปอร์เซนต์</label>
-                                <div class="form-group">
-                                <div class="input-group mb-3">
-                                    <input type="text" id="percent" class="form-control" placeholder="199.40">
-                                    <div class="input-group-append">
-                                    <span class="input-group-text">%</span>
+                        <br>
+                        <div class="row">
+                           <div class="col-lg-4 offset-lg-1">
+                              <div class="form-group">
+                                 <label class="control-label">จำนวนเงิน</label>
+                                 <div class="form-group">
+                                    <div class="input-group mb-3">
+                                       <div class="input-group-prepend">
+                                          <span class="input-group-text">฿</span>
+                                       </div>
+                                       <input type="text" id="money" class="form-control" placeholder="10000">
+                                       <div class="input-group-append">
+                                          <span class="input-group-text">.00</span>
+                                       </div>
                                     </div>
-                                </div>
-                                </div>
-                            </div>
+                                 </div>
+                              </div>
+                           </div>
+                           <div class="col-lg-4 offset-lg-2">
+                              <div class="form-group">
+                                 <label class="control-label">ส่วนลดเปอร์เซนต์</label>
+                                 <div class="form-group">
+                                    <div class="input-group mb-3">
+                                       <input type="text" id="percent" class="form-control" placeholder="199.40">
+                                       <div class="input-group-append">
+                                          <span class="input-group-text">%</span>
+                                       </div>
+                                    </div>
+                                 </div>
+                              </div>
+                           </div>
                         </div>
-                    </div>
-
-                    <hr>
-
-                    <button onclick="javascript:pexxrcent();" type="button" class="btn btn-success">คำนวณเงิน</button>
-
-                    </form>
-                    </div>
-                    </div>
-                </div>
-              </div>
-
-              
-              </div>
-              <br>
-              <h6><center>COPYRIGHT © 2019 ALL SYSTEM BY FEWEIEI</center></h6>
-        <script src="asset/js/jquery.min.js"></script>
-        <script src="asset/js/func.js"></script>
-        <script src="asset/js/sweetalert.min.js"></script>
-</body>
+                        <hr>
+                        <button onclick="javascript:pexxrcent();" type="button" class="btn btn-success">คำนวณเงิน</button>
+                     </form>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <br>
+      <h6>
+         <center>COPYRIGHT © 2019 ALL SYSTEM BY FEWEIEI</center>
+      </h6>
+      <script src="asset/js/jquery.min.js"></script>
+      <script src="asset/js/func.js"></script>
+      <script src="asset/js/sweetalert.min.js"></script>
+   </body>
 </html>
